f76747567c3a1085f6d80ad64d73f78a27d34712
huzaifasddiquii <139953680+huzaifasddiquii@users.noreply.github.com>
2023-08-04 20:07:09 +0530

    Add files via upload

---
+++ wordcounter.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class wordcounter{
+    public static void main(String[] args) {
+        System.out.println("Type your text: ");
+        Scanner sc = new Scanner(System.in);
+        String userInput = sc.nextLine();
+        sc.close();
+        userInput = userInput.trim();
+        int count = 0;
+        if (userInput.length() == 0){
+            count = 0;
+        }
+        else{
+            // count the first word
+            // it must exist because the first charcter
+            // must not be a space
+            count++;
+            for(int i=0; i<userInput.length(); i++){
+                if (userInput.charAt(i) == ' '
+                && userInput.charAt(i + 1) != ' '){
+                    // '?'
+                    count++;
+                }
+            }
+        }
+        System.out.println("Word count: "+ count);
+
+    }
+}
